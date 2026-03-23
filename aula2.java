b956b59c778a14430a00667dff29a8c39385c208
viniciusEvangelist <viniciusy222@gmail.com>
2026-03-23 10:59:02 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,5 +1,5 @@
 public class OOLana { 
-        public static void main(String[] args) {
+       /* public static void main(String[] args) {
                 Scanner lerNumero = new Scanner(System.in);
                 System.out.println("digite o valor de x: ");
                 float x = lerNumero.nextFloat();
@@ -9,6 +9,20 @@ public class OOLana {
                 System.out.println("o valor da subtração de x - y é: "+(x-y));
                 System.out.println("o valor da multiplicação de x * y é: "+(x*y));
                 System.out.println("o valor da divisão de x/y é: " + (x/y));
-                System.out.println("o valor da x^y é: "+(Math.pow(x,y))); */
-         }
+                System.out.println("o valor da x^y é: "+(Math.pow(x,y))); 
+         }*/
+       /* public static void main(String[] args){
+                Scanner lerFloat = new Scanner(System.in);
+                System.out.println("este programa calcula o valor da expressao a*a = (b*b) + (c*c)");
+                System.out.println("digite o valor de a:  ");
+                float a = lerFloat.nextFloat();
+                System.out.println("digite o valor de b: ");
+                float b = lerFloat.nextFloat();
+                System.out.println("digite o valor de c: ");
+                float c = lerFloat.nextFloat();
+                    if (a*a == (b*b)+(c*c))
+                    System.out.println("(b*b)+(c*c) = " + a*a);
+                    else System.out.println("digite valores que atendam a expressao a*a = (b*b) + (c*c)");
+                return; 
+        } */
 }       
