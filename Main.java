02d3e8d8c8ef0283551cf1e6476e91bc773eb9e1
Vinicius Evangelista Borges Pinto <viniciusy222@gmail.com>
2026-06-16 15:16:37 -0300

    primeira calculadora

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float x = 10;
+        float y = 5;
+        System.out.println(calculadora.somar(x, y));
+        System.out.println(calculadora.subtracao(x, y));
+        System.out.println(calculadora.multiplicacao(x, y));
+        System.out.println(calculadora.divisao(x, y));
+    }
+}
---
+++ calculadora.java
@@ -0,0 +1,19 @@
+public class calculadora {
+    static float somar(float a, float b){
+        float c = a + b;
+        return c;
+    }
+    static float subtracao(float a, float b){
+        float c = a - b;
+        return c;
+    }
+    static float multiplicacao(float a, float b){
+        float c = a*b;
+        return c;
+    }
+    static float divisao(float a, float b){
+        float c = a/b;
+        return c;
+    }
+}
+
