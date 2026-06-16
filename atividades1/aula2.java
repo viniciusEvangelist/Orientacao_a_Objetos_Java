4deca5060fcd41962ec57246060990319fd36a61
Vinicius Evangelista Borges Pinto <viniciusy222@gmail.com>
2026-06-16 15:02:21 -0300

    Refactor main method and clean up commented code

--- atividades1/aula2.java
+++ atividades1/aula2.java
@@ -1,5 +1,5 @@
 public class OOLana { 
-       /* public static void main(String[] args) {
+       public static void main(String[] args) {
                 Scanner lerNumero = new Scanner(System.in);
                 System.out.println("digite o valor de x: ");
                 float x = lerNumero.nextFloat();
@@ -10,23 +10,23 @@ public class OOLana {
                 System.out.println("o valor da multiplicação de x * y é: "+(x*y));
                 System.out.println("o valor da divisão de x/y é: " + (x/y));
                 System.out.println("o valor da x^y é: "+(Math.pow(x,y))); 
-         }*/
-       /*public static void main(String[] args){
-        JOptionPane.showMessageDialog(null, "Esse programa calcula o valor da expressão a*a = (b*b)+(c*c)");
-        String valor1 = JOptionPane.showInputDialog("digite o valor de a: ");
-        int a = Integer.parseInt(valor1);
-        String valor2 = JOptionPane.showInputDialog("digite o valor de b: ");
-        int b = Integer.parseInt(valor2);
-        String valor3 = JOptionPane.showInputDialog("digite o valor de c ");
-        int c = Integer.parseInt(valor3);
-        if ((b*b)+(c*c)==a*a) {
-           JOptionPane.showMessageDialog(null, "substituindo os valores de a, b e c na expressão \n" +
-                   "a*a = (b*b)+(c*c) \n" +
-                   "temos "+ a+"*"+a+" = "+ b+"*"+b +"+"+ c+"*"+c+ "\n"+
-                   "o valor da expressão é: " +a*a);
-        }
-        else {
-           JOptionPane.showMessageDialog(null, "digite um valor válido" );
-        }
-       } */
+         }
+       public static void main(String[] args){
+               JOptionPane.showMessageDialog(null, "Esse programa calcula o valor da expressão a*a = (b*b)+(c*c)");
+               String valor1 = JOptionPane.showInputDialog("digite o valor de a: ");
+               int a = Integer.parseInt(valor1);
+               String valor2 = JOptionPane.showInputDialog("digite o valor de b: ");
+               int b = Integer.parseInt(valor2);
+               String valor3 = JOptionPane.showInputDialog("digite o valor de c ");
+               int c = Integer.parseInt(valor3);
+               if ((b*b)+(c*c)==a*a) {
+                  JOptionPane.showMessageDialog(null, "substituindo os valores de a, b e c na expressão \n" +
+                          "a*a = (b*b)+(c*c) \n" +
+                          "temos "+ a+"*"+a+" = "+ b+"*"+b +"+"+ c+"*"+c+ "\n"+
+                          "o valor da expressão é: " +a*a);
+               }
+               else {
+                  JOptionPane.showMessageDialog(null, "digite um valor válido" );
+               }
+        } 
 }       
